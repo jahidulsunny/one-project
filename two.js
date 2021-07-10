743228324856716ea7c097ed66e57b0b3fe1f149
jahidulsunny <jahidulsunny705@gmail.com>
2021-07-10 07:44:42 +0600

    added one call back function one our two.js file

--- two.js
+++ two.js
@@ -1,20 +1,63 @@
 //071 Higher Order Function in Javascript
 
-function add(a,b){
-    return a+b
+function add(a, b) {
+    return a + b
 }
 
-function manipulate(a,b,func){
-    var c=a+b
-    var d=a-b
-    function multiple(){
-        var m=func(a,b)
-        return c*d*m
+function manipulate(a, b, func) {
+    var c = a + b
+    var d = a - b
+    function multiple() {
+        var m = func(a, b)
+        return c * d * m
     }
     return multiple
 }
 
-var result=manipulate(3,4,add)
+var result = manipulate(3, 4, add)
 console.log(result())
 
-   
+//higher order function 2nd time practise
+
+function run(a, b) {
+    return a * b
+}
+
+function manipulate(a, b, func) {
+    var c = a + b
+    var d = a - b
+    return function () {
+        var m = func(a, b)
+        return c * d * m
+    }
+}
+
+var result = manipulate(3, 4, run)
+console.log(result())
+
+//072 Closure First Look in Javascript | Stack Learner
+
+var a = 10
+
+function forCloser() {
+    console.log(a)
+}
+forCloser()  //when a function block use his previous variable on his block which called closer
+
+//073 Callback Function in Javascript | Stack Learner
+function sample(a, b, cb) {
+    var c = a + b
+    var d = a - b
+    var result = cb(c, d)
+    return result
+}
+
+var addition = sample(3, 4, function (c, d) {
+    return c + d
+})
+console.log(addition)
+
+var another = sample(3, 4, function (c, d) {
+    return c - d
+})
+console.log(another)
