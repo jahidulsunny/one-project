b7865939870f02c5946568cdff341e2510f38b5b
jahidulsunny <jahidulsunny705@gmail.com>
2021-07-10 08:40:50 +0600

    added map method implementing

--- two.js
+++ two.js
@@ -63,23 +63,46 @@ var another = sample(3, 4, function (c, d) {
 console.log(another)
 
 //074 Foreach Implementation in Javascript | Stack Learner
-var arr=[1,2,3,4,5,6,7,8,9]
-var arr1=[2,4,6,8]
- var sum=0
-arr.forEach(function(value,index,arr){
-    sum+=value
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+var arr1 = [2, 4, 6, 8]
+var sum = 0
+arr.forEach(function (value, index, arr) {
+    sum += value
 })
 console.log(sum)
-     //Implementing of forEach method
-function myForEach(arr,cb){
-    for(i=0;i<arr.length;i++){
-       cb(arr[i],i,arr)
+//Implementing of forEach method
+function myForEach(arr, cb) {
+    for (i = 0; i < arr.length; i++) {
+        cb(arr[i], i, arr)
     }
 }
-myForEach(arr,function(value,index,arr){
-    console.log(value,index,arr)
+myForEach(arr, function (value, index, arr) {
+    console.log(value, index, arr)
 })
 
-myForEach(arr1,function(value,index,arr){
-    console.log(value,index,arr)
+myForEach(arr1, function (value, index, arr) {
+    console.log(value, index, arr)
 })
+
+//075 Map Function in Javascript | Stack Learner
+
+var arrMap = [1, 2, 3, 4, 5, 6, 7]
+
+var sqr = arrMap.map(function (value, index, arr) {
+    return value = Math.floor(Math.random() * 100)
+})
+console.log(sqr)
+   //implementing map method in javascript
+function myMap(arr,cb){
+    newArr=[]
+    for(i=0;i<arr.length;i++){
+        var temp=cb(arr[i],i,arr)
+        newArr.push(temp)
+    }
+    return newArr
+} 
+
+var sqr1=myMap(arrMap,function(value,index,arr){
+    return value*value
+})
+console.log(sqr1)
